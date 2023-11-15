cd5415ae396303e77f6dd849dee2c0053efe6d81
Shiaba <>
2023-11-15 19:03:51 +0000

    Add admin review & comment settings setup

--- review/admin.py
+++ review/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import review
+from .models import review, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(review)
 class ReviewAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on', 'author')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('created_on', 'approved')
+    search_fields = ('name', 'body', 'email')
+    actions = ['approve_comments']
