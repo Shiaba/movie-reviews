71d1753a882dd778bf30f92465bef21f1589468a
Shiaba <>
2023-11-16 18:57:21 +0000

    Add footer to base.html & style it, import generic to views.py

--- review/views.py
+++ review/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import review
 
-# Create your views here.
+
+class MovieList(generic.ListView):
+    model = review
+    queryset = review.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 6
