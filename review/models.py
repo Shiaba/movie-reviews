035c2c43e88228c891096b0829a7fcc946498673
Shiaba <>
2023-11-20 17:45:47 +0000

    Add basic create-review page & clean up style from index

--- review/models.py
+++ review/models.py
@@ -8,14 +8,16 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class review(models.Model):
     title = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=150, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='published_review')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='published_review')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='review_likes', blank=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -33,7 +35,8 @@ class Comment(models.Model):
     name = models.CharField(max_length=80)
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey(review, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        review, on_delete=models.CASCADE, related_name='comments')
 
     class Meta:
         ordering = ['created_on']
