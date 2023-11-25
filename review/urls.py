4c8efcbaa7ff64ac5b078189d28a8fcfbe4d5f8a
Shiaba <>
2023-11-25 23:47:30 +0000

    Change spelling error & add to url & buttons

--- review/urls.py
+++ review/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
          views.ReviewDetail.as_view(), name='review_detail'),
     path('like/<slug:slug>', views.ReviewLike.as_view(), name='review_like'),
     path('create_review/', views.CreateReview.as_view(), name='create_review'),
-    path('update_review/', views.CreateReview.as_view(), name='update_review'),
+    path('update_review/<slug:slug>',
+         views.CreateReview.as_view(), name='update_review'),
 ]
