7625b2924dca5eb616336a5acc5773b7bb592ec1
Shiaba <>
2023-11-25 23:16:23 +0000

    Add update_review url & add button that only right author user can see

--- review/urls.py
+++ review/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
          views.ReviewDetail.as_view(), name='review_detail'),
     path('like/<slug:slug>', views.ReviewLike.as_view(), name='review_like'),
     path('create_review/', views.CreateReview.as_view(), name='create_review'),
+    path('update_review/', views.CreateReview.as_view(), name='update_review'),
 ]
