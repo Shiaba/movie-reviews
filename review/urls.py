74b2a668ab6195644f0e51b3c7055682f0bc11a1
Shiaba <>
2023-11-25 11:36:15 +0000

    Change path name in urls.py in order to fix page error

--- review/urls.py
+++ review/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.MovieList.as_view(), name='home'),
-    path('<slug:slug>/', views.ReviewDetail.as_view(), name='review_detail'),
+    path('review_detail/<slug:slug>/',
+         views.ReviewDetail.as_view(), name='review_detail'),
     path('like/<slug:slug>', views.ReviewLike.as_view(), name='review_like'),
     path('create_review/', views.CreateReview.as_view(), name='create_review'),
 ]
